4b870f2bc0e5775f4aa59cc064451b5ccf9d3d60
dollop <dollop@gmail.com>
2016-05-05 14:22:50 -0700

    First commit

---
+++ DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  HackingWithSwift_Project01
+//
+//  Created by Robert Burns on 5/3/16.
+//  Copyright © 2016 dollop Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
